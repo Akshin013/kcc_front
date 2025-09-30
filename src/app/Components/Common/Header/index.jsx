763a6124fc57f78cbf5a2b4e4f398943a457ad80
Akshin013 <akshingadimov@gmail.com>
2025-09-30 13:19:04 +0400

    Update index.jsx

--- src/app/Components/Common/Header/index.jsx
+++ src/app/Components/Common/Header/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className='bg-[#333333] flex border border-[#ebc032] rounded-lg justify-center gap-8 p-2.5'>
-        <div>
+        <Link href='/'>
             {/* <img src='/front/public/logo.svg' alt='Logo' className='h-3 w-3'/> */}
             <img src="https://res.cloudinary.com/dvm6my9na/image/upload/v1759055745/KCC_LOGO_otkfkn.jpg" alt="Логотип KCC" className="h-12 w-auto" />
-        </div>
-        <h1>Head</h1>
+        </Link>
     </div>
   )
 }
