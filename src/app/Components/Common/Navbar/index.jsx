251c88ff4f23b023386973f0bdadb1460025cfe2
Akshin013 <akshingadimov@gmail.com>
2025-09-30 17:03:30 +0400

    Update index.jsx

--- src/app/Components/Common/Navbar/index.jsx
+++ src/app/Components/Common/Navbar/index.jsx
@@ -7,26 +7,34 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import Link from "next/link";
 
 const Navbar = () => {
-
-  
   const [isOpen, setIsOpen] = useState(false);
 
   const links = [
-    { href: '/', icon: <IoMdHome size={32} color='#ebc032'/>, label: 'ESAS', color: '#ebc032' },
-    { href: '/Favorites', icon: <FaHeart size={38} color='#e62328'/>, label: 'SECILMISDER', color: '#e62328' },
-    { href: '/about', icon: <TbInfoOctagonFilled size={32} color='#ebc032'/>, label: 'HAKKIMIZDA', color: '#ebc032' },
+    { href: '/', icon: <IoMdHome size={37} color='#ebc032'/>, label: 'ESAS', color: '#ebc032' },
+    { href: '/Favorites', icon: <FaHeart size={46} color='#e62328'/>, label: 'SECILMISDER', color: '#e62328', elevated: true },
+    { href: '/about', icon: <TbInfoOctagonFilled size={37} color='#ebc032'/>, label: 'HAKKIMIZDA', color: '#ebc032' },
   ];
-// bg-[#333333]
+
   return (
     <>
       {/* Мобильная навигация (нижняя панель) */}
       <div className="bg-[#333333] z-50 rounded-lg border border-gray-700 fixed bottom-0 left-0 w-full grid grid-cols-3 gap-3 md:hidden">
         {links.map((link, i) => (
-          <Link key={i} href={link.href} className={`flex flex-col items-center gap-0.5 rounded-2xl p-2`}>
-            {link.icon}
-            <p className="text-xs" style={{color: link.color}}>{link.label}</p>
-          </Link>
+  <Link 
+  key={i} 
+  href={link.href} 
+  className={`flex flex-col items-center gap-0.5 rounded-2xl p-2 ${link.elevated ? '-mt-7' : ''}`}
+>
+  {link.icon}
+  <p className={`text-xs ${link.elevated ? 'mt-4.5' : ''}`} style={{color: link.color}}>
+    {link.label}
+  </p>
+</Link>
+
         ))}
+        {/* <Link href='' className='bg-red-700'><IoMdHome size={37} color='#ebc032'/></Link>
+        <Link href='' className='bg-red-700'></Link>
+        <Link href='' className='bg-red-700'></Link> */}
       </div>
 
       {/* ПК: бургер-меню */}
