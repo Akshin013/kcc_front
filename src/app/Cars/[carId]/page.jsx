3dbce75ab4a0361b3da0ffcf197d8f6306551e52
Akshin013 <akshingadimov@gmail.com>
2025-09-30 15:48:56 +0400

    Update page.jsx

--- src/app/Cars/[carId]/page.jsx
+++ src/app/Cars/[carId]/page.jsx
@@ -127,7 +127,7 @@ const CarDetail = () => {
         </div>
 
         <a
-          
+          // href={whatsappLink}
           target="_blank"
           rel="noopener noreferrer"
           className="mt-4 inline-flex items-center gap-2 bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600"
